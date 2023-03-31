ee51646050bc7def6ff936e39917d14888039af4
frderoubaix <francois.deroubaix@outlook.fr>
2023-03-31 22:27:56 +0200

    feat: change api-gateway with spring cloud

--- api-gateway/src/main/java/fr/insa/apigateway/ApiGatewayApplication.java
+++ api-gateway/src/main/java/fr/insa/apigateway/ApiGatewayApplication.java
@@ -3,10 +3,8 @@ package fr.insa.apigateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @SpringBootApplication
-@EnableZuulProxy
 @EnableDiscoveryClient
 public class ApiGatewayApplication {
 
